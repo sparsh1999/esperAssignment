5842fa29ec3305a92feee43ddfa15e489fe0a170
sparsh_singhal <sparshsinghal95@gmail.com>
2021-05-16 12:39:21 +0530

    IMP Driver code, api call, saving data to db, load features and populate in tabs, load options when page clicked

--- app/src/main/java/com/example/esperassisgnment/Activity/MainActivity.java
+++ app/src/main/java/com/example/esperassisgnment/Activity/MainActivity.java
@@ -9,30 +9,54 @@ import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.example.esperassisgnment.Adapters.OptionAdapter;
+import com.example.esperassisgnment.App;
+import com.example.esperassisgnment.Database.dao.ExclusionDAO;
+import com.example.esperassisgnment.Database.dao.FeatureDAO;
+import com.example.esperassisgnment.Database.dao.OptionsDAO;
+import com.example.esperassisgnment.Database.dao.SelectionDAO;
 import com.example.esperassisgnment.Helpers.SelectionManager;
 import com.example.esperassisgnment.Models.Data;
+import com.example.esperassisgnment.Models.Entities.Exclusions;
+import com.example.esperassisgnment.Models.Entities.Feature;
+import com.example.esperassisgnment.Models.Entities.Options;
+import com.example.esperassisgnment.Models.Entities.Selection;
+import com.example.esperassisgnment.Models.Responses.DataResponse;
+import com.example.esperassisgnment.Models.Responses.ExclusionResponse;
+import com.example.esperassisgnment.Models.Responses.FeatureResponse;
 import com.example.esperassisgnment.R;
 import com.google.android.material.tabs.TabLayout;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.reflect.TypeToken;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
     RecyclerView recyclerView;
     TabLayout featureTabs;
     List<Data> dataList = new ArrayList<>();
+    List<Feature> features = new ArrayList<>();
     ProgressDialog networkProgress ;
     OptionAdapter optionsAdapter;
     SelectionManager selectionManager;
+    App app;
+    String TAG = "MAIN_ACTIVITY";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        app = App.getAppInstance();
         selectionManager = new SelectionManager();
         recyclerView = findViewById(R.id.recyclerView);
         featureTabs = findViewById(R.id.featureTabs);
@@ -44,16 +68,18 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.setLayoutManager(layoutManager);
 
         if(isNetworkAvailable()){
+            Log.d(TAG, "Network is Available");
             requestDataFromServer();
         }
         else{
-            loadData();
+            loadFeatures();
         }
 
         featureTabs.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
             @Override
             public void onTabSelected(TabLayout.Tab tab) {
-
+                Log.d(TAG, "tab clicked at "+tab.getPosition() +" "+tab.getText());
+                loadOptions(features.get(tab.getPosition()).getId());
             }
 
             @Override
@@ -71,21 +97,121 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    protected void onPause() {
+        super.onPause();
+        hideNetworkProgress();
+    }
+
     private void requestDataFromServer(){
+        showNetworkProgress();
+        try {
+            app.api.requestData().enqueue(new Callback<DataResponse>() {
+                @Override
+                public void onResponse(Call<DataResponse> call, Response<DataResponse> response) {
+                    DataResponse dataResponse = response.body();
 
+                    // TODO there should be some better way to parse exclusionResponse
+                    if (dataResponse!=null) {
+                        for (JsonElement jsonElement: dataResponse.exclusionData){
+                            if (jsonElement instanceof JsonArray){
+                                Log.d(TAG, jsonElement.toString()+" "+((JsonArray) jsonElement).size());
+                                List<Selection> exclusions = app.gson.fromJson(jsonElement,
+                                        new TypeToken<List<Selection>>(){}.getType());
+                                dataResponse.exclusionResponses.add(new ExclusionResponse(exclusions));
+                            }
+                        }
+                        saveData(dataResponse);
+                        loadFeatures();
+                    }
+                    else{
+                        Log.d(TAG, "Data Response is Empty or null");
+                    }
+                }
+
+                @Override
+                public void onFailure(Call<DataResponse> call, Throwable t) {
+                    Log.d(TAG, "Network Failure"+t.getMessage());
+                }
+            });
+        }
+        catch (Exception e){
+            Log.d(TAG, e.getMessage());
+        }
+        hideNetworkProgress();
     }
 
-    private void loadData(){
 
+    private void saveData(DataResponse response){
+        // get all DAO objects
+        showNetworkProgress();
+        ExclusionDAO exclusionDAO = app.db.getExclusionsDAO();
+        SelectionDAO selectionDAO = app.db.getSelectionDAO();
+        FeatureDAO featureDAO = app.db.getFeatureDAO();
+        OptionsDAO optionsDAO = app.db.getOptionsDAO();
+
+        // delete all data before saving new one
+        optionsDAO.deleteAll();
+        featureDAO.deleteAll();
+        selectionDAO.deleteAll();
+        // exclusions should automatically delete due to foreign key constraints !!! TODO CHECK
+        // add features and options
+        if (response.features!=null)
+        for (FeatureResponse featureResponse: response.features){
+            Feature feature = new Feature();
+            feature.setId(featureResponse.featureId);
+            feature.setName(featureResponse.name);
+            featureDAO.insert(feature);
+
+            if (featureResponse.options!=null){
+                for (Options option: featureResponse.options){
+                    option.setFeatureId(feature.getId());
+                    optionsDAO.insert(option);
+                }
+            }
+        }
+
+        // now add exclusions Data
+
+        if (response.exclusionResponses!=null)
+        for (ExclusionResponse exclusionResponse: response.exclusionResponses){
+            if (exclusionResponse.exclusion!=null && exclusionResponse.exclusion.size()==2){
+                int selectionId1 = (int)selectionDAO.insert(exclusionResponse.exclusion.get(0));
+                int selectionId2 = (int)selectionDAO.insert(exclusionResponse.exclusion.get(0));
+                Exclusions exclusions = new Exclusions(selectionId1, selectionId2);
+                exclusionDAO.insert(exclusions);
+            }
+            else{
+                Log.d(TAG, "exclusion Response is either null or <2");
+            }
+        }
+        hideNetworkProgress();
     }
+
+    private void loadOptions(int featureId){
+        OptionsDAO optionsDAO = app.db.getOptionsDAO();
+        List<Options> options = optionsDAO.getAllOptionsForFeature(featureId);
+        optionsAdapter.updateOptions(options);
+    }
+
+    private void loadFeatures(){
+        showNetworkProgress();
+        FeatureDAO featureDAO = app.db.getFeatureDAO();
+        List<Feature> features = featureDAO.getAllFeatures();
+        if (features!=null){
+            this.features.clear();
+            this.features.addAll(features);
+        }
+        addTabs();
+        hideNetworkProgress();
+    }
+
 
     private void addTabs(){
-        networkProgress.show();
         featureTabs.removeAllTabs();
-        for (Data data: dataList){
-            featureTabs.addTab(featureTabs.newTab().setText(data.feature.getName()));
+        for (Feature feature: features){
+            featureTabs.addTab(featureTabs.newTab().setText(feature.getName()));
         }
-        networkProgress.hide();
     }
 
     private boolean isNetworkAvailable() {
@@ -93,5 +219,13 @@ public class MainActivity extends AppCompatActivity {
                 = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
         return activeNetworkInfo != null && activeNetworkInfo.isConnected();
+    }
+
+    private void showNetworkProgress(){
+        if (!networkProgress.isShowing()) networkProgress.show();
+    }
+
+    private void hideNetworkProgress(){
+        if (networkProgress.isShowing()) networkProgress.hide();
     }
 }
--- app/src/main/java/com/example/esperassisgnment/Helpers/Constants.java
+++ app/src/main/java/com/example/esperassisgnment/Helpers/Constants.java
@@ -5,4 +5,5 @@ public class Constants {
     public static final String OPTIONS_TABLE = "OPTIONS";
     public static final String EXCLUSION_TABLE = "EXCLUSIONS";
     public static final String SELECTION_TABLE = "SELECTION";
+    public static final String BASE_URL = "https://my-json-server.typicode.com/mhrpatel12/";
 }
